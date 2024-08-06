36f197a98a040516faa2032b1591acbe7ff28153
Monika <laiskutis.monikai@gmail.com>
2024-08-06 10:23:21 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Features } from './components/features/Features';
+import { Footer } from './components/footer/Footer';
+import { Header } from './components/header/Header';
+import { Hero } from './components/hero/Hero';
+import { Sponsors } from './components/sponsors/Sponsors';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <Features />
+        <Sponsors />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
