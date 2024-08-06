355c454df897dca7e9243f62cba1d99df8cb5013
Monika <laiskutis.monikai@gmail.com>
2024-08-06 14:57:19 +0300

    components

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,37 +1,27 @@
+import { Feature } from './Feature';
+import style from './Features.module.css';
 
 export function Features() {
     return (
-        <section>
-            <div>
-                <div>💡</div>
-                <h2>Instant Server Start</h2>
-                <p>On demand file serving over native ESM, no bundling required!</p>
-            </div>
-            <div>
-                <div>⚡️</div>
-                <h2>Lightning Fast HMR</h2>
-                <p>Hot Module Replacement (HMR) that stays fast regardless of app size.</p>
-            </div>
-            <div>
-                <div>🛠️</div>
-                <h2>Rich Features</h2>
-                <p>Out-of-the-box support for TypeScript, JSX, CSS and more.</p>
-            </div>
-            <div>
-                <div>📦</div>
-                <h2>Optimized Build</h2>
-                <p>Pre-configured Rollup build with multi-page and library mode support.</p>
-            </div>
-            <div>
-                <div>🔩</div>
-                <h2>Universal Plugins</h2>
-                <p>Rollup-superset plugin interface shared between dev and build.</p>
-            </div>
-            <div>
-                <div>🔑</div>
-                <h2>Fully Typed APIs</h2>
-                <p>Flexible programmatic APIs with full TypeScript typing.</p>
-            </div>
+        <section className={style.featuresList}>
+            <Feature
+                icon="💡" title="Instant Server Start"
+                description="On demand file serving over native ESM, no bundling required!" />
+            <Feature
+                icon="⚡️" title="Lightning Fast HMR"
+                description="Hot Module Replacement (HMR) that stays fast regardless of app size." />
+            <Feature
+                icon="🛠️" title="Rich Features"
+                description="Out-of-the-box support for TypeScript, JSX, CSS and more." />
+            <Feature
+                icon="📦" title="Optimized Build"
+                description="Pre-configured Rollup build with multi-page and library mode support." />
+            <Feature
+                icon="🔩" title="Universal Plugins"
+                description="Rollup-superset plugin interface shared between dev and build." />
+            <Feature
+                icon="🔑" title="Fully Typed APIs"
+                description="Flexible programmatic APIs with full TypeScript typing." />
         </section>
     );
 }
